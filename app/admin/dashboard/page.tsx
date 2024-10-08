81a9cd147f1f9ac28863c8b53003cddeb32d1d5a
Shreejan Acharya <achars@pointclickcare.com>
2024-10-08 00:35:09 -0400

    company layout setup completed

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+    return (<>
+        Welcome to the admin dashboard
+    </>);
+}
---
+++ app/admin/layout.tsx
@@ -0,0 +1,80 @@
+"use client"
+import React from 'react';
+import {
+    BankOutlined,
+    CarOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Dashboard', '1', <PieChartOutlined />),
+    getItem('Companies', '2', <BankOutlined />),
+    getItem('Driver', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Vehicle', '2', <CarOutlined />),
+];
+
+const App: React.FC = ({ children }: React.PropsWithChildren) => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible >
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        {children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default App;
